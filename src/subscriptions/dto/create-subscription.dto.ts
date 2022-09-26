bd1023368fc35abbddb357b5caaf8e45f410af83
Seong-Jin Kim <sys.ryan0902@gmail.com>
2022-09-26 19:04:11 +0900

    feat: createSubsctipion
    - conflice exception 처리 작업 필요

--- src/subscriptions/dto/create-subscription.dto.ts
+++ src/subscriptions/dto/create-subscription.dto.ts
@@ -1,1 +1,45 @@
-export class CreateSubscriptionDto {}
+import { IsEnum, IsNotEmpty, IsNumber, IsString } from "class-validator";
+import { BLOCKCHAIN_EVENT_ENUM } from "src/common/enums/event.enum";
+
+export class CreateSubscriptionRequestDto {
+  /**
+   * 구독할 이벤트의 토픽
+   */
+  @IsEnum(BLOCKCHAIN_EVENT_ENUM, { each: true })
+  // TODO:  string[] 대신 ENUM 사용한 이유 설명 작성
+  topics: BLOCKCHAIN_EVENT_ENUM[];
+
+  /**
+   * 이벤트를 구독할 스마트 컨트랙트의 주소
+   */
+  @IsString({ message: "Invalid contractAddress." })
+  @IsNotEmpty({ message: "contractAddress must not be empty" })
+  contractAddress: string;
+}
+
+export class CreateSubscriptionResponseDto {
+  /**
+   * 구독 id (:subscription-id)
+   */
+  id: number;
+
+  /**
+   * 구독한 이벤트의 토픽
+   */
+  topics: string[];
+
+  /**
+   * 이벤트를 구독한 스마트 컨트랙트의 주소
+   */
+  contractAddress: string;
+
+  /**
+   * 구독 생성일시. 서버에서는 Date 객체로 다루지만 응답은 string 으로 내려준다
+   */
+  createdAt: Date;
+
+  /**
+   * 구독 최종 수정일시. 서버에서는 Date 객체로 다루지만 응답은 string 으로 내려준다
+   */
+  updatedAt: Date;
+}
--- src/subscriptions/subscriptions.service.ts
+++ src/subscriptions/subscriptions.service.ts
@@ -1,26 +1,49 @@
-import { Injectable } from '@nestjs/common';
-import { CreateSubscriptionDto } from './dto/create-subscription.dto';
-import { UpdateSubscriptionDto } from './dto/update-subscription.dto';
+import { Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Repository } from "typeorm";
+import {
+  CreateSubscriptionRequestDto,
+  CreateSubscriptionResponseDto,
+} from "./dto/create-subscription.dto";
+import { Subscriptions } from "./entities/subscription.entity";
 
 @Injectable()
 export class SubscriptionsService {
-  create(createSubscriptionDto: CreateSubscriptionDto) {
-    return 'This action adds a new subscription';
+  constructor(
+    @InjectRepository(Subscriptions) private subscriptionsRepository: Repository<Subscriptions>
+  ) {}
+
+  async createSubscription(
+    createSubscriptionRequestDto: CreateSubscriptionRequestDto
+  ): Promise<CreateSubscriptionResponseDto> {
+    // TODO: 구독 중복 체크 (Error: 409 Conflict)
+    // contractAddress가 존재하고 && topic이 모두 일치할 때 충돌
+
+    // 구독 정보 DB 저장
+    const subscription = await this.subscriptionsRepository.create(createSubscriptionRequestDto);
+    const newSubscription = await this.subscriptionsRepository.save(subscription);
+
+    // 구독 정보 응답
+    return newSubscription;
   }
 
-  findAll() {
-    return `This action returns all subscriptions`;
-  }
+  // create(createSubscriptionDto: CreateSubscriptionDto) {
+  //   return 'This action adds a new subscription';
+  // }
 
-  findOne(id: number) {
-    return `This action returns a #${id} subscription`;
-  }
+  // findAll() {
+  //   return `This action returns all subscriptions`;
+  // }
 
-  update(id: number, updateSubscriptionDto: UpdateSubscriptionDto) {
-    return `This action updates a #${id} subscription`;
-  }
+  // findOne(id: number) {
+  //   return `This action returns a #${id} subscription`;
+  // }
 
-  remove(id: number) {
-    return `This action removes a #${id} subscription`;
-  }
+  // update(id: number, updateSubscriptionDto: UpdateSubscriptionDto) {
+  //   return `This action updates a #${id} subscription`;
+  // }
+
+  // remove(id: number) {
+  //   return `This action removes a #${id} subscription`;
+  // }
 }
