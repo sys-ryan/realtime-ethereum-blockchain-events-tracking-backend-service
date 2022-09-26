6274859fda644478e6f51a4dade4779525014961
Seong-Jin Kim <sys.ryan0902@gmail.com>
2022-09-26 19:04:11 +0900

    feat: createSubsctipion
    - conflice exception 처리 작업 필요

--- src/subscriptions/entities/subscription.entity.ts
+++ src/subscriptions/entities/subscription.entity.ts
@@ -1,1 +1,26 @@
-export class Subscription {}
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from "typeorm";
+
+@Entity()
+export class Subscriptions {
+  // TODO: Column 정보 detail 작성
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: "simple-array" })
+  topics: string[];
+
+  @Column()
+  contractAddress: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
--- src/subscriptions/subscriptions.module.ts
+++ src/subscriptions/subscriptions.module.ts
@@ -1,9 +1,12 @@
-import { Module } from '@nestjs/common';
-import { SubscriptionsService } from './subscriptions.service';
-import { SubscriptionsController } from './subscriptions.controller';
+import { Module } from "@nestjs/common";
+import { SubscriptionsService } from "./subscriptions.service";
+import { SubscriptionsController } from "./subscriptions.controller";
+import { Subscriptions } from "./entities/subscription.entity";
+import { TypeOrmModule } from "@nestjs/typeorm";
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Subscriptions])],
   controllers: [SubscriptionsController],
-  providers: [SubscriptionsService]
+  providers: [SubscriptionsService],
 })
 export class SubscriptionsModule {}
