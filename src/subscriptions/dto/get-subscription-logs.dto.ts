8053d5adfc70cdc8243b6c438e643431a2ba7bcd
Seong-Jin Kim <sys.ryan0902@gmail.com>
2022-09-28 03:38:58 +0900

    docs: Swagger API Documentation

--- src/subscriptions/dto/get-subscription-logs.dto.ts
+++ src/subscriptions/dto/get-subscription-logs.dto.ts
@@ -1,12 +1,16 @@
+import { ApiProperty, ApiPropertyOptional } from "@nestjs/swagger";
 import { Transform } from "class-transformer";
 import { IsEnum, IsNumber, IsOptional, Max, Min } from "class-validator";
 import { ChainEventLog } from "src/chain-event-log/entities/chain-event-log.entity";
 import { SORT_ENUM } from "src/common/enums/log.enum";
 
 export class GetSubscriptionLogsQueryDto {
+  @ApiPropertyOptional({ default: SORT_ENUM.DESC })
   @IsEnum(SORT_ENUM)
   readonly sort: SORT_ENUM = SORT_ENUM.DESC;
 
+  @ApiPropertyOptional()
+  @ApiProperty({ default: null, minimum: 1000000000000, maximum: 9999999999999 })
   @IsOptional()
   @IsNumber()
   @Min(1000000000000)
@@ -19,6 +23,8 @@ export class GetSubscriptionLogsQueryDto {
   })
   start: number | null = null;
 
+  @ApiPropertyOptional()
+  @ApiProperty({ default: null, minimum: 1000000000000, maximum: 9999999999999 })
   @IsOptional()
   @IsNumber()
   @Min(1000000000000)
@@ -31,10 +37,12 @@ export class GetSubscriptionLogsQueryDto {
   })
   end: number | null = null;
 
+  @ApiPropertyOptional()
   @IsNumber()
   @Transform(({ value }) => +value)
   offset: number = 0;
 
+  @ApiPropertyOptional()
   @IsNumber()
   @Transform(({ value }) => +value)
   limit: number = 50;
@@ -69,16 +77,19 @@ export class GetSubscriptionLogsResponseDto {
   /**
    * request 의 sort 파라미터 값
    */
+  @ApiProperty({ enum: SORT_ENUM })
   sort: "asc" | "desc";
 
   /**
    * request 의 start 파라미터 값. unix timestamp (밀리초. 13 자리)
    */
+  @ApiProperty({ default: null, minimum: 1000000000000, maximum: 9999999999999 })
   start: number | null;
 
   /**
-   * request 의 end 파라미터 값. unix timestamp (밀리10 자리)
+   * request 의 end 파라미터 값. unix timestamp (밀리초. 13 자리)
    */
+  @ApiProperty({ default: null, minimum: 1000000000000, maximum: 9999999999999 })
   end: number | null;
 
   /**
