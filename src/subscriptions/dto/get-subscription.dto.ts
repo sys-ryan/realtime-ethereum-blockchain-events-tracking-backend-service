685747bb8a87d15b045d99d8473981334e4ed1ab
Seong-Jin Kim <sys.ryan0902@gmail.com>
2022-09-27 23:56:11 +0900

    feat: 구독에서 발생한 로그 조회 API

--- src/subscriptions/dto/get-subscription.dto.ts
+++ src/subscriptions/dto/get-subscription.dto.ts
@@ -5,13 +5,11 @@ export class GetSubscriptionResponseDto {
   createdAt: Date;
   updatedAt: Date;
 
-  // TODO: 해당 구독의 로그 수 체크
   /**
    * 서버에 저장된 해당 구독의 로그 수
    */
   logSize: number;
 
-  // TODO: 첫 번째 로그 timestamp 얻기
   /**
    * 첫 번째 로그의 timestamp
    */
