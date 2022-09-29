a51547ea89c2ba3d843a79d8f8870cd33529651c
Seong-Jin Kim <sys.ryan0902@gmail.com>
2022-09-29 18:24:47 +0900

    test: 구독 생성
    - contractAddress가 존재하지만 topics의 elements가 다를 경우 새로운 구독 생성
    - contractAddress가 존재하고, topics의 elements가 일치할 경우 ConflictException

--- src/subscriptions/subscriptions.service.spec.ts
+++ src/subscriptions/subscriptions.service.spec.ts
@@ -1,16 +1,51 @@
 import { Test, TestingModule } from "@nestjs/testing";
 import { getRepositoryToken } from "@nestjs/typeorm";
+import { BLOCKCHAIN_EVENT_ENUM } from "../common/enums/event.enum";
 import { DataSource } from "typeorm";
 import { ChainEventLogService } from "../chain-event-log/chain-event-logs.service";
 import { ChainEventLog } from "../chain-event-log/entities/chain-event-log.entity";
 import { Subscriptions } from "./entities/subscription.entity";
 import { SubscriptionsService } from "./subscriptions.service";
+import { ConflictException } from "@nestjs/common";
+
+const subscriptions: Subscriptions[] = [];
+
+const chainEventLogs: ChainEventLog[] = [];
 
 const mockChainEventLogRepository = () => ({});
 
-const mockSubscriptionsRepository = () => ({});
+const mockSubscriptionsRepository = () => ({
+  find: jest.fn().mockImplementation(async (query) => {
+    const where = query.where;
 
-const mockChainEventLogsService = () => ({});
+    let existingSubscriptions: Subscriptions[] = [];
+
+    if (where.contractAddress) {
+      existingSubscriptions = subscriptions.filter((subscription) => {
+        return subscription.contractAddress === where.contractAddress;
+      });
+    }
+
+    return existingSubscriptions;
+  }),
+  findOne: jest.fn(),
+  create: jest.fn().mockImplementation((subscriptionDto) => {
+    return subscriptionDto;
+  }),
+  save: jest.fn().mockImplementation(async (subscription) => {
+    const newSubscription = {
+      id: Math.random(),
+      ...subscription,
+    };
+    subscriptions.push(newSubscription);
+
+    return newSubscription;
+  }),
+});
+
+const mockChainEventLogsService = () => ({
+  startEventTracking: jest.fn(),
+});
 
 const mockDataSource = () => ({});
 
@@ -31,11 +66,11 @@ describe("SubscriptionsService", () => {
         },
         {
           provide: ChainEventLogService,
-          useValue: mockChainEventLogsService,
+          useValue: mockChainEventLogsService(),
         },
         {
           provide: DataSource,
-          useValue: mockDataSource,
+          useValue: mockDataSource(),
         },
       ],
     }).compile();
@@ -43,7 +78,54 @@ describe("SubscriptionsService", () => {
     service = module.get<SubscriptionsService>(SubscriptionsService);
   });
 
+  afterEach(async () => {
+    subscriptions.splice(0, subscriptions.length);
+    chainEventLogs.splice(0, chainEventLogs.length);
+  });
+
   it("should be defined", () => {
     expect(service).toBeDefined();
+  });
+
+  const DAI_CONTRACT_ADDRESS = "0x6B175474E89094C44Da98b954EedeAC495271d0F";
+
+  describe("구독 생성", () => {
+    it("contractAddress가 존재하지만 topics의 elements가 다를 경우 새로운 구독 생성", async () => {
+      await service.createSubscription({
+        contractAddress: DAI_CONTRACT_ADDRESS,
+        topics: [BLOCKCHAIN_EVENT_ENUM.TRANSFER],
+      });
+
+      expect(subscriptions).toHaveLength(1);
+
+      await service.createSubscription({
+        contractAddress: DAI_CONTRACT_ADDRESS,
+        topics: [BLOCKCHAIN_EVENT_ENUM.TRANSFER, BLOCKCHAIN_EVENT_ENUM.APPROVAL],
+      });
+
+      expect(subscriptions).toHaveLength(2);
+    });
+
+    it("contractAddress가 존재하고, topics의 elements가 일치할 경우 ConflictException", async () => {
+      await service.createSubscription({
+        contractAddress: DAI_CONTRACT_ADDRESS,
+        topics: [BLOCKCHAIN_EVENT_ENUM.TRANSFER],
+      });
+
+      expect(subscriptions).toHaveLength(1);
+
+      await expect(
+        service.createSubscription({
+          contractAddress: DAI_CONTRACT_ADDRESS,
+          topics: [BLOCKCHAIN_EVENT_ENUM.TRANSFER],
+        })
+      ).rejects.toBeInstanceOf(ConflictException);
+
+      expect(subscriptions).toHaveLength(1);
+    });
+  });
+
+  describe("구독 정보 조회", () => {
+    it(" 존재하지 않는 subscription-id 일 경우 NotFoundException", async () => {});
   });
 });
